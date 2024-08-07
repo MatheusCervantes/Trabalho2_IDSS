7a63eec0a83fac531ba0d5cd0786852ac2310e4f
MatheusCervantes <matheuscervantes1@gmail.com>
2024-08-07 13:43:22 -0300

    Att erros

--- app/src/db.php
+++ app/src/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "senha_da_nasa";
 $dbname = "carrosDB";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
